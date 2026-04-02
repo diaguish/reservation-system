613d2517c88fc8ecd526318bc7ff311995ab00c9
Diago Alioune Tall <diagoalioune.tall@gmail.com>
2026-04-02 11:19:17 +0200

    mise en place de ConferencePricing.java

--- src/main/java/com/reservation/model/ConferencePricing.java
+++ src/main/java/com/reservation/model/ConferencePricing.java
@@ -1,5 +1,24 @@
 package com.reservation.model;
 
-public class ConferencePricing {
-    
+public class ConferencePricing implements PricingStrategy {
+
+    @Override
+    public double calculerPrix(Reservable reservable, long duree) {
+        if (!(reservable instanceof ConferenceRoom)) {
+            throw new IllegalArgumentException(
+                "ConferencePricing attend un ConferenceRoom");
+        }
+        
+        ConferenceRoom room = (ConferenceRoom) reservable;
+        
+
+        double prix = room.getBaseConference();
+        
+       
+        if (duree > room.getDureeFixe()) {
+            prix += (duree - room.getDureeFixe()) * room.getExtraParHeure();
+        }
+        
+        return prix;
+    }
 }
