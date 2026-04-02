79e01fcc593abd02cf4605766cc21d3ff62d8deb
Diago Alioune Tall <diagoalioune.tall@gmail.com>
2026-04-02 10:00:26 +0200

    Test SSH Keys

---
+++ src/main/java/com/reservation/model/ConferenceRoom.java
@@ -0,0 +1,11 @@
+package com.reservation.model;
+
+public class ConferenceRoom {
+    private String id;
+    private String nom;
+    private int capacite;   
+    private double baseConference;
+    private double facteur;
+    private PricingStrategy strategy;
+    
+}
